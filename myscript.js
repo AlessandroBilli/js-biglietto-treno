af97497addc773bdafa08207b151907faf9d676e
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-06 17:07:00 +0100

    fix: structure js

--- myscript.js
+++ myscript.js
@@ -1,12 +1,14 @@
 'use script'
 
 const distanza = parseInt (prompt ('Inserisci distanza in km'));
-console.log (distanza);
+console.log ('Distanza passeggero' distanza);
 
 
 const età = parseInt (prompt ('Inserisci la tua età'));
-console.log (età);
+console.log ('Età passeggero' età);
 
 const prezzo = distanza * 0.21;
 console.log('Prezzo del biglietto:', prezzo);
+
+
 
