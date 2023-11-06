395e9760ac4f3ca7f0fe58b5f557eff0cbe8f8a7
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-06 16:59:27 +0100

    feat: log js/html and start js structure

---
+++ myscript.js
@@ -0,0 +1,9 @@
+'use script'
+
+const distanza = parseInt (prompt ('Inserisci distanza in km'));
+console.log (distanza);
+
+
+const età = parseInt (prompt ('Inserisci la tua età'));
+console.log (età);
+
