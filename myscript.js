cf3b8f6e0380c84c46bf159a54f4544a358d3931
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-06 17:02:47 +0100

    feat: prezzo bilgietto

--- myscript.js
+++ myscript.js
@@ -7,3 +7,6 @@ console.log (distanza);
 const età = parseInt (prompt ('Inserisci la tua età'));
 console.log (età);
 
+const prezzo = distanza * 0.21;
+console.log('Prezzo del biglietto:', prezzo);
+
