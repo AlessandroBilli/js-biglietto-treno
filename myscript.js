16e69dea872770913029958e93f80780d3c8cc11
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-06 17:27:19 +0100

    feat: prezzo biglietto minorenni

--- myscript.js
+++ myscript.js
@@ -7,8 +7,21 @@ console.log ('Distanza passeggero', distanza);
 const età = parseInt (prompt ('Inserisci la tua età'));
 console.log ('Età passeggero', età);
 
+
+
 const prezzo = distanza * 0.21;
 console.log('Prezzo del biglietto:', prezzo);
+
+if (età < 18) {
+    const percentualeVenti = 20;
+    const sconto = (prezzo * percentualeVenti) / 100;
+    console.log('Sconto del 20%:', sconto);
+    const prezzoScontato = prezzo - sconto;
+    console.log('Prezzo con lo sconto:', prezzoScontato);
+  }
+
+
+
 
 
 
